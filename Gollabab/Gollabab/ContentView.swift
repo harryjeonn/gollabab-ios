367ca61c3306691ccd0054fef7c7ba6b18caf010
Hoxvi <jeonhyeonseong@jeonhyeonseongui-MacBook-Pro.local>
2022-04-28 23:09:30 +0900

    Add #4 - 새 프로젝트 생성
    This closes #4

---
+++ Gollabab/Gollabab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gollabab
+//
+//  Created by 전현성 on 2022/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gollabab/Gollabab/GollababApp.swift
@@ -0,0 +1,17 @@
+//
+//  GollababApp.swift
+//  Gollabab
+//
+//  Created by 전현성 on 2022/04/28.
+//
+
+import SwiftUI
+
+@main
+struct GollababApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
