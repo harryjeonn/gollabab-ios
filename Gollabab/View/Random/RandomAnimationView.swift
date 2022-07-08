268646ece515ead71017482dc8789502aa6c3240
Harry <jeongus97@gmail.com>
2022-07-08 17:55:13 +0900

    Feat #102 - 랜덤 애니메이션 화면 기본 UI 구현 (기능 X)
    * 장소 불러오기, UI까지만 했음
    
    * 랜덤 애니메이션, 랜덤기능 마무리

--- Gollabab/View/Random/RandomAnimationView.swift
+++ Gollabab/View/Random/RandomAnimationView.swift
@@ -11,8 +11,52 @@ struct RandomAnimationView: View {
     @ObservedObject var viewModel: RandomViewModel
     
     var body: some View {
-        ForEach(viewModel.selectedItem, id: \.self) { item in
-            Text(item.rawValue)
+        VStack(spacing: 0) {
+            Text("여기서 골라밥")
+                .font(.eliceBold(size: 30))
+                .foregroundColor(.primaryBeige)
+                .padding(.bottom, 12)
+            
+            Text("우주의 기운을 모아서... 멈춰!")
+                .font(.eliceP3())
+                .foregroundColor(.secondaryPink)
+                .padding(.bottom, 65)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    Image("card_clover")
+                        .resizable()
+                        .frame(width: 120, height: 180)
+                    
+                    Image("card_diamond")
+                        .resizable()
+                        .frame(width: 120, height: 180)
+                    
+                    Image("card_heart")
+                        .resizable()
+                        .frame(width: 120, height: 180)
+                }
+                .padding(EdgeInsets(top: 0, leading: 21, bottom: 110, trailing: 21))
+            }
+            
+            Button {
+                // 투표결과 보여주기
+            } label: {
+                Text("멈춰!")
+                    .font(.eliceP1())
+                    .foregroundColor(.white)
+                    .frame(minWidth: .zero, maxWidth: .infinity)
+                    .frame(height: 48)
+                    .background(Color.secondaryRed)
+                    .cornerRadius(100)
+                    .padding(EdgeInsets(top: 40, leading: 27, bottom: 27, trailing: 32))
+            }
+            
+        }
+        .frame(minWidth: .zero, maxWidth: .infinity, minHeight: .zero, maxHeight: .infinity)
+        .background(Color.text300)
+        .onAppear {
+            viewModel.fetchPlace()
         }
     }
 }
