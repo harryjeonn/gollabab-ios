5981058dabb101b653c19acd8455dbd3d2bdec2a
Harry <jeongus97@gmail.com>
2022-07-26 19:43:42 +0900

    Fix #160 - 폰트 사이즈 수정
    * 기기별로 폰트사이즈가 조정되어 fix값으로 수정

--- Gollabab/Utils/FontExtension.swift
+++ Gollabab/Utils/FontExtension.swift
@@ -9,46 +9,46 @@ import SwiftUI
 
 extension Font {
     static func eliceBold(size: CGFloat) -> Font {
-        return Font.custom("EliceDigitalBaeumOTF-Bd", size: size)
+        return Font.custom("EliceDigitalBaeumOTF-Bd", fixedSize: size)
     }
     
     static func elice(size: CGFloat) -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: size)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: size)
     }
     
     static func eliceP1() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF-Bd", size: 18)
+        return Font.custom("EliceDigitalBaeumOTF-Bd", fixedSize: 18)
     }
     
     static func eliceP2() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF-Bd", size: 17)
+        return Font.custom("EliceDigitalBaeumOTF-Bd", fixedSize: 17)
     }
     
     static func eliceP2Regular() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: 17)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: 17)
     }
     
     static func eliceP3() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: 16)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: 16)
     }
     
     static func eliceP4() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: 15)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: 15)
     }
     
     static func eliceCaption() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: 14)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: 14)
     }
     
     static func eliceCaptionSmall() -> Font {
-        return Font.custom("EliceDigitalBaeumOTF", size: 12)
+        return Font.custom("EliceDigitalBaeumOTF", fixedSize: 12)
     }
     
     static func aggroTitle() -> Font {
-        return Font.custom("OTSBAggroM", size: 36)
+        return Font.custom("OTSBAggroM", fixedSize: 36)
     }
     
     static func aggroBold(_ size: CGFloat) -> Font {
-        return Font.custom("OTSBAggroB", size: size)
+        return Font.custom("OTSBAggroB", fixedSize: size)
     }
 }
